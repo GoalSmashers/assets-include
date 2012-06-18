7b149d8d9a50c959806616398064a5224d0a1ddf
Jakub Pawlowicz <contact@jakubpawlowicz.com>
2012-06-18 13:46:23 +0200

    Finished binary with tests.

---
+++ test/binary-test.js
@@ -0,0 +1,40 @@
+var vows = require('vows'),
+  assert = require('assert'),
+  exec = require('child_process').exec;
+
+var binaryContext = function(options, context) {
+  context.topic = function() {
+    exec("cd test; ../bin/assetsinc " + options, this.callback);
+  };
+  return context;
+};
+
+exports.commandsSuite = vows.describe('binary commands').addBatch({
+  'no options': binaryContext('', {
+    'should output help': function(error, stdout) {
+      assert.equal(/usage:/.test(stdout), true);
+    }
+  }),
+  'help options': binaryContext('-h', {
+    'should output help': function(error, stdout) {
+      assert.equal(/usage:/.test(stdout), true);
+    }
+  }),
+  'plain scripts': binaryContext('-r ./data/public -c ./data/config.yml javascripts/all.js', {
+    'must give script inclusions': function(error, stdout) {
+      assert.equal(stdout.match(/<script/g).length, 3);
+      assert.equal(stdout.match(/(one|two|three)\.js\?\d+/g).length, 3);
+    }
+  }),
+  'bundled scripts': binaryContext('-b -r ./data/public -c ./data/config.yml javascripts/all.js', {
+    'must give bundled script inclusion': function(error, stdout) {
+      assert.equal(stdout.match(/<script/g).length, 1);
+      assert.equal(stdout.match(/all\.js\?\d+/g).length, 1);
+    }
+  }),
+  'bundled and inlined scripts': binaryContext('-b -i -r ./data/public -c ./data/config.yml javascripts/all.js', {
+    'must give bundled script inclusion': function(error, stdout) {
+      assert.equal(stdout, "<script>123</script>");
+    }
+  })
+});
