f53149fe261533147595a9c837f245a43c31a2a8
Jakub Pawlowicz <jakub@goalsmashers.com>
2014-02-27 22:13:35 +0000

    Removes optimist in favor to commander for CLI options parsing.

--- test/binary-test.js
+++ test/binary-test.js
@@ -17,12 +17,12 @@ var binaryContext = function(options, context) {
 exports.commandsSuite = vows.describe('binary commands').addBatch({
   'no options': binaryContext('', {
     'should output help': function(error, stdout) {
-      assert.equal(/usage:/.test(stdout), true);
+      assert.equal(/Usage:/.test(stdout), true);
     }
   }),
   'help option': binaryContext('-h', {
     'should output help': function(error, stdout) {
-      assert.equal(/usage:/.test(stdout), true);
+      assert.equal(/Usage:/.test(stdout), true);
     }
   }),
   'version option': binaryContext('-v', {
@@ -31,59 +31,59 @@ exports.commandsSuite = vows.describe('binary commands').addBatch({
       assert.equal(stdout, version + '\n');
     }
   }),
-  'plain scripts': binaryContext('-r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'plain scripts': binaryContext('--root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give script inclusions': function(error, stdout) {
       assert.equal(stdout.match(/<script/g).length, 3);
       assert.equal(stdout.match(/(one|two|three)\.js\?\d+/g).length, 3);
     }
   }),
-  'bundled scripts': binaryContext('-b -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled scripts': binaryContext('--bundled --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal(stdout.match(/<script/g).length, 1);
       assert.equal(stdout.match(/ (async|defer)>/g), null);
       assert.equal(stdout.match(/all\.js\?\d+/g).length, 1);
     }
   }),
-  'bundled scripts with loading mode': binaryContext('-b -r ./data/public -c ./data/config.yml -m async javascripts/all.js', {
+  'bundled scripts with loading mode': binaryContext('--bundled --root ./data/public --config ./data/config.yml --loading-mode async javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal(stdout.match(/<script/g).length, 1);
       assert.equal(stdout.match(/ async>/g).length, 1);
       assert.equal(stdout.match(/all\.js\?\d+/g).length, 1);
     }
   }),
-  'bundled scripts with asset hosts': binaryContext('-b -a assets[0,1].goalsmashers.com -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled scripts with asset hosts': binaryContext('--bundled --asset-hosts assets[0,1].goalsmashers.com --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal(stdout.match(/<script/g).length, 1);
       assert.equal(stdout.match(/\/\/assets0.goalsmashers.com\/javascripts\/bundled\/all\.js\?\d+/g).length, 1);
     }
   }),
-  'bundled scripts with cache boosters': binaryContext('-b -s -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled scripts with cache boosters': binaryContext('--bundled --cache-boosters --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal(stdout.match(/<script/g).length, 1);
       assert.equal(stdout.match(/"\/javascripts\/bundled\/all-test1234567\.js"/g).length, 1);
     }
   }),
-  'bundled and inlined scripts': binaryContext('-b -i -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled and inlined scripts': binaryContext('--bundled --inline --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal(stdout, '<script>123</script>\n');
     }
   }),
-  'plain lists': binaryContext('-l -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'plain lists': binaryContext('--list --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert(/^\/javascripts\/one\.js\?\d+,\/javascripts\/two\.js\?\d+,\/javascripts\/three\.js\?\d+$/.test(stdout));
     }
   }),
-  'bundled plain lists': binaryContext('-l -b -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled plain lists': binaryContext('--list --bundled --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert(/^\/javascripts\/bundled\/all\.js\?\d+$/.test(stdout));
     }
   }),
-  'bundled plain lists with asset hosts': binaryContext('-l -b -a assets[3,2,1].goalsmashers.com -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'bundled plain lists with asset hosts': binaryContext('--list --bundled --asset-hosts assets[3,2,1].goalsmashers.com --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert(/^\/\/assets3.goalsmashers.com\/javascripts\/bundled\/all\.js\?\d+$/.test(stdout));
     }
   }),
-  'inline with cache boosters': binaryContext('-i -b -s -r ./data/public -c ./data/config.yml javascripts/all.js', {
+  'inline with cache boosters': binaryContext('--inline --bundled --cache-boosters --root ./data/public --config ./data/config.yml javascripts/all.js', {
     'must give bundled script inclusion': function(error, stdout) {
       assert.equal('<script>123-booster</script>\n', stdout);
     }
